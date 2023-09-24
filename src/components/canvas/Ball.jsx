a66bc70ec0a0a174ab608081d9ee11b2e8af5509
Aubrey Kurten <aekurten@gmail.com>
2023-09-24 18:14:19 +0200

    almost done

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,3 +1,5 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
 import {
   Decal,
   Float,
@@ -5,34 +7,51 @@ import {
   Preload,
   useTexture,
 } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import React, { Suspense } from "react";
+
 import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
-  return <div>Ball</div>;
+
+  return (
+    <Float speed={1.75} rotationIntensity={0} floatIntensity={2}>
+      <ambientLight intensity={2} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+    </Float>
+  );
 };
 
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
       frameloop="demand"
-      shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers isMobile={isMobile} />
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
       </Suspense>
+
       <Preload all />
     </Canvas>
   );
 };
 
-export default Ball;
+export default BallCanvas;
