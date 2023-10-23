b80ff30434246b5617602919d59423d69916e3a0
Aubrey Kurten <aekurten@gmail.com>
2023-10-23 15:11:25 +0200

    Fixed some mobile issues

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,6 +5,14 @@ import { styles } from "../style";
 import { EarthCanvas } from "./canvas";
 import SectionWrapper from "../hoc/SectionWrapper";
 import { slideIn } from "../utils/motion";
+import { RxMobile, RxEnvelopeClosed } from "react-icons/rx";
+import {
+  AiOutlineFacebook,
+  AiOutlineGithub,
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+  AiOutlineWhatsApp,
+} from "react-icons/ai";
 
 const Contact = () => {
   const formRef = useRef();
@@ -25,51 +33,71 @@ const Contact = () => {
       >
         <p className={`${styles.sectionSubText}`}>Get in touch</p>
         <h3 className={`${styles.sectionHeadText}`}>Contact.</h3>
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className="mt-12 flex flex-col gap-8"
-        >
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your name?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary rounded-lg outline-none border-none font-medium "
-            />
-          </label>
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Email</span>
-            <input
-              type="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your email?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary rounded-lg outline-none border-none font-medium "
-            />
-          </label>
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your message</span>
-            <textarea
-              rows={7}
-              name="message"
-              value={form.message}
-              onChange={handleChange}
-              placeholder="What do you want to say?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary rounded-lg outline-none border-none font-medium "
-            />
-          </label>
-          <button
-            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
-            type="submit"
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
+        <div className="flex flex-col gap-4">
+          <div>
+            <p className={`font-light ${styles.sectionSubText}`}>Cell</p>
+            <a
+              href="tel:+27823372457"
+              className="flex items-center gap-2 hover:text-blue-600 transition-all duration-300 font-semibold text-xl mt-2"
+            >
+              <RxMobile size={24} />
+              +27 82 337 2457
+            </a>
+          </div>
+          <div>
+            <p className={`font-light ${styles.sectionSubText}`}>Email</p>
+            <a
+              href="mailto:aekurten@gmail.com"
+              className="flex items-center gap-2 hover:text-blue-600 transition-all duration-300 font-semibold text-xl mt-2"
+            >
+              <RxEnvelopeClosed size={24} />
+              aekurten@gmail.com
+            </a>
+          </div>
+          <div>
+            <p className={`font-light ${styles.sectionSubText}`}>Whatsapp</p>
+            <a
+              href="whatsapp://send?phone=+27823372457&text=Hi Aubrey,"
+              className="flex items-center gap-2 hover:text-blue-600 transition-all duration-300 font-semibold text-xl mt-2"
+            >
+              <AiOutlineWhatsApp size={24} />
+              Let's Chat
+            </a>
+          </div>
+          <div>
+            <p className={`font-light ${styles.sectionSubText}`}>Socials</p>
+            <div className="flex gap-3 mt-2">
+              <a
+                href="https://www.facebook.com/profile.php?id=100008234941918"
+                target="_blank"
+                className="hover:text-blue-600 transition-all duration-300"
+              >
+                <AiOutlineFacebook size={32} />
+              </a>
+              <a
+                href="https://www.instagram.com/_aubrey_kurten_/"
+                target="_blank"
+                className="hover:text-blue-600 transition-all duration-300"
+              >
+                <AiOutlineInstagram size={32} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/aubrey-kurten-8a97b11b9/"
+                target="_blank"
+                className="hover:text-blue-600 transition-all duration-300"
+              >
+                <AiOutlineLinkedin size={32} />
+              </a>
+              <a
+                href="https://github.com/AEKurten"
+                target="_blank"
+                className="hover:text-blue-600 transition-all duration-300"
+              >
+                <AiOutlineGithub size={32} />
+              </a>
+            </div>
+          </div>
+        </div>
       </motion.div>
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
