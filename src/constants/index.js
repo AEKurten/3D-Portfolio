18dad2ed6ba4f92e7732f946b77c82906431ae19
Aubrey Kurten <aekurten@gmail.com>
2024-01-30 07:35:19 +0200

    Updated a few things

--- src/constants/index.js
+++ src/constants/index.js
@@ -176,33 +176,6 @@ const experiences = [
   },
 ];
 
-const testimonials = [
-  {
-    testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
-];
-
 const projects = [
   {
     name: "Exceed IT - Dashboard",
@@ -223,7 +196,7 @@ const projects = [
       },
     ],
     image: exceedItDash,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/AEKurten",
   },
   {
     name: "Job Search App",
@@ -244,7 +217,7 @@ const projects = [
       },
     ],
     image: jobit,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/AEKurten",
   },
   {
     name: "Fair Share Bill Splitter",
@@ -265,7 +238,7 @@ const projects = [
       },
     ],
     image: fairshare,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/AEKurten",
   },
   {
     name: "Anime Vault",
@@ -286,7 +259,7 @@ const projects = [
       },
     ],
     image: Anime,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/AEKurten",
   },
 ];
 
